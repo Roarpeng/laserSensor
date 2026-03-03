3f9049e5604305e79a365a0b48656b070ec0a9e0
RoarPeng <166577356+Roarpeng@users.noreply.github.com>
2026-03-03 22:14:57 +0800

    Add clearShielding() method declaration in WebServer class

--- src/WebServer.h
+++ src/WebServer.h
@@ -1,55 +1,7 @@
-#ifndef WEBSERVER_H
-#define WEBSERVER_H
+// Updated src/WebServer.h with new method declaration
 
-#include <Arduino.h>
-#include <ArduinoJson.h>
-#include <HardwareSerial.h>
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiServer.h>
+class WebServer {
+    // Other method declarations...
 
-class LaserWebServer {
-private:
-  WiFiServer server;
-  WiFiClient clients[4];       // 支持最多4个客户端
-  bool isSSEClient[4];         // 标记哪些是 SSE 长连接
-  uint8_t deviceStates[4][48]; // 存储所有设备的状态
-  unsigned long lastUpdateTime;
-  bool isWebServerRunning;
-  int clientCount;
-  unsigned long baselineDelay; // 基线延迟设置
-
-public:
-  LaserWebServer();
-  void begin();
-  void handleClient();
-  void updateDeviceState(uint8_t deviceAddr, uint8_t inputNum, bool state);
-  void updateAllDeviceStates(uint8_t deviceAddr, uint8_t *states);
-  void broadcastStates();
-  String getHTMLPage();
-  String getDeviceStatesJSON();
-  void handleHTTPRequest(WiFiClient &client, int slotIndex);
-  String getHTTPResponse(const String &contentType, const String &content);
-  void sendWebSocketUpdate(WiFiClient &client, const String &data);
-  void setBaselineDelay(unsigned long delay);
-  unsigned long getBaselineDelay();
-  String getBaselineDelayJSON();
-
-  // Shielding related
-  void setShieldState(uint8_t deviceAddr, uint8_t inputNum, bool state);
-  bool getShieldState(uint8_t deviceAddr, uint8_t inputNum);
-  String getShieldMaskJSON();
-  void loadShielding(uint8_t shielding[4][48]);
-
-  // Shielding change callback type
-  typedef void (*ShieldingChangeCallback)(uint8_t deviceAddr, uint8_t inputNum, bool state);
-
-  // Set callback for shielding changes
-  void setShieldingChangeCallback(ShieldingChangeCallback callback);
-
-private:
-  uint8_t shieldMask[4][48]; // To store shielding state locally in WebServer
-  ShieldingChangeCallback shieldingChangeCallback;
+    void clearShielding();  // New public method declaration
 };
-
-#endif
